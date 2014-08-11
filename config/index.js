131dbb6c4aab35a258f64d264a633f016cd2d582
Alexander Scott <xander@cadcoder.com>
2014-08-11 20:36:56 +1000

    Testing Config Profile

--- config/index.js
+++ config/index.js
@@ -1,10 +1,14 @@
 var config;
+
 switch (process.env.NODE_ENV) {
 
+    case 'testing':
+        config = require('./testing.json');
+        break;
     case 'production':
         config = require('./production.json');
         break;
-
+    case 'development':
     default:
         config = require('./development.json');
 }
