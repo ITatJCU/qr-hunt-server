3a36271d6e551e1ece95c1b981b9eb3af93b164f
Alexander Scott <xander@cadcoder.com>
2014-08-12 20:26:02 +1000

    Code Data Structure Naming fix, DAO Create

--- test/dao/test-sequelize-code-dao.js
+++ test/dao/test-sequelize-code-dao.js
@@ -20,5 +20,31 @@ module.exports = {
         var dao = new DAO(Code);
         test.equals(Code, dao.modelDefinition());
         test.done();
+    },
+    testCreate: function (test) {
+        var dao = new DAO(Code);
+
+        var qr = {
+            title: 'IT@JCU Central Display',
+            content: 'Need we say more?',
+            locationX: 123.12,
+            locationY: 321.23,
+            gameIndex: 0
+        };
+
+        dao.create(qr, function (result, err) {
+            test.ok(result, 'Code not created or returned. Database Error.' + err);
+
+            if (result) {
+                test.equals(qr.title, result.title);
+                test.equals(qr.content, result.content);
+                test.equals(qr.locationX, result.locationX);
+                test.equals(qr.locationY, result.locationY);
+                test.equals(qr.gameIndex, result.gameIndex);
+            }
+
+            test.done();
+        });
+
     }
 };
