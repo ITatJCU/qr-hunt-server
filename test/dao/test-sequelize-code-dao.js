af3c8fa00619f3c43ac01e1eb37d62cf9d014f3f
Alexander Scott <xander@cadcoder.com>
2014-08-12 20:39:15 +1000

    CodeDAO AllGameCodes

--- test/dao/test-sequelize-code-dao.js
+++ test/dao/test-sequelize-code-dao.js
@@ -4,6 +4,32 @@ var Code = models.Code;
 var DAO = require('../../lib/dao/sequelize-code-dao');
 
 
+var qrCodes = [
+    {
+        title: 'IT@JCU Main Display',
+        content: 'Core of the Fun',
+        locationX: 1.50,
+        locationY: 1.50,
+        gameIndex: 1
+    },
+    {
+        title: 'Structural Engineering Display',
+        content: 'Foundational Practice',
+        locationX: 88.2,
+        locationY: 1598.182,
+        gameIndex: 2
+    },
+    {
+        title: 'Creative Arts',
+        content: 'Bringing a bit of flair'
+    },
+    {
+        title: 'Mystery Prize',
+        content: 'What could it be?',
+        gameIndex: 3
+    },
+];
+
 module.exports = {
     setUp: function (callback) {
 
@@ -46,34 +72,8 @@ module.exports = {
             test.done();
         });
     },
-    testAll: function(test){
+    testAll: function (test) {
         var dao = new DAO(Code);
-
-        var qrCodes = [
-            {
-                title: 'IT@JCU Main Display',
-                content: 'Core of the Fun',
-                locationX: 1.50,
-                locationY: 1.50,
-                gameIndex: 1
-            },
-            {
-                title: 'Structural Engineering Display',
-                content: 'Foundational Practice',
-                locationX: 88.2,
-                locationY: 1598.182,
-                gameIndex: 2
-            },
-            {
-                title: 'Creative Arts',
-                content: 'Bringing a bit of flair'
-            },
-            {
-                title: 'Mystery Prize',
-                content: 'What could it be?',
-                gameIndex: 3
-            },
-        ];
 
         var i = 0;
         var insertFunction = function () {
@@ -82,9 +82,9 @@ module.exports = {
             } else {
                 dao.all(function (codes, err) {
 
-                    test.equals(codes.length,4);
+                    test.equals(codes.length, 4);
 
-                    test.equals(codes[0].title,  'IT@JCU Main Display');
+                    test.equals(codes[0].title, 'IT@JCU Main Display');
                     test.equals(codes[0].content, 'Core of the Fun');
                     test.equals(codes[0].locationX, 1.50);
                     test.equals(codes[0].locationY, 1.50);
@@ -107,6 +107,30 @@ module.exports = {
                     test.equals(codes[3].locationX, null);
                     test.equals(codes[3].locationY, null);
                     test.equals(codes[3].gameIndex, 3);
+
+                    test.done();
+                });
+            }
+            i++;
+        };
+
+        insertFunction();
+    },
+
+    testAllGameCodes: function (test) {
+        var dao = new DAO(Code);
+
+        var i = 0;
+        var insertFunction = function () {
+            if (i < qrCodes.length) {
+                dao.create(qrCodes[i], insertFunction);
+            } else {
+                dao.allGameCodes(function (codes, err) {
+
+                    test.equals(codes.length, 3);
+                    test.equals(codes[0].title, 'IT@JCU Main Display');
+                    test.equals(codes[1].title, 'Structural Engineering Display');
+                    test.equals(codes[2].title, 'Mystery Prize');
 
                     test.done();
                 });
