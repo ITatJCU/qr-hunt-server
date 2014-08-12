48ab7cf1540c3966f281dfffb133c52f31a30957
Alexander Scott <xander@cadcoder.com>
2014-08-12 20:35:09 +1000

    Code DAO All

--- test/dao/test-sequelize-code-dao.js
+++ test/dao/test-sequelize-code-dao.js
@@ -45,6 +45,75 @@ module.exports = {
 
             test.done();
         });
+    },
+    testAll: function(test){
+        var dao = new DAO(Code);
 
+        var qrCodes = [
+            {
+                title: 'IT@JCU Main Display',
+                content: 'Core of the Fun',
+                locationX: 1.50,
+                locationY: 1.50,
+                gameIndex: 1
+            },
+            {
+                title: 'Structural Engineering Display',
+                content: 'Foundational Practice',
+                locationX: 88.2,
+                locationY: 1598.182,
+                gameIndex: 2
+            },
+            {
+                title: 'Creative Arts',
+                content: 'Bringing a bit of flair'
+            },
+            {
+                title: 'Mystery Prize',
+                content: 'What could it be?',
+                gameIndex: 3
+            },
+        ];
+
+        var i = 0;
+        var insertFunction = function () {
+            if (i < qrCodes.length) {
+                dao.create(qrCodes[i], insertFunction);
+            } else {
+                dao.all(function (codes, err) {
+
+                    test.equals(codes.length,4);
+
+                    test.equals(codes[0].title,  'IT@JCU Main Display');
+                    test.equals(codes[0].content, 'Core of the Fun');
+                    test.equals(codes[0].locationX, 1.50);
+                    test.equals(codes[0].locationY, 1.50);
+                    test.equals(codes[0].gameIndex, 1);
+
+                    test.equals(codes[1].title, 'Structural Engineering Display');
+                    test.equals(codes[1].content, 'Foundational Practice');
+                    test.equals(codes[1].locationX, 88.2);
+                    test.equals(codes[1].locationY, 1598.182);
+                    test.equals(codes[1].gameIndex, 2);
+
+                    test.equals(codes[2].title, 'Creative Arts');
+                    test.equals(codes[2].content, 'Bringing a bit of flair');
+                    test.equals(codes[2].locationX, null);
+                    test.equals(codes[2].locationY, null);
+                    test.equals(codes[2].gameIndex, 0);
+
+                    test.equals(codes[3].title, 'Mystery Prize');
+                    test.equals(codes[3].content, 'What could it be?');
+                    test.equals(codes[3].locationX, null);
+                    test.equals(codes[3].locationY, null);
+                    test.equals(codes[3].gameIndex, 3);
+
+                    test.done();
+                });
+            }
+            i++;
+        };
+
+        insertFunction();
     }
 };
