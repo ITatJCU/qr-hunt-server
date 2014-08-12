e645d2e93eb8f418c29dfcd3945934f2f286be06
Alex <Alex@SKYCATCHER.(none)>
2014-08-12 15:47:48 +1000

    Refined Player Test

--- test/dao/test-player.js
+++ test/dao/test-player.js
@@ -10,11 +10,9 @@ module.exports = {
         Player.find(fingerprint)
             .failure(function (err) {
                 Player.create({
-                    name: playerAlias,
+                    alias: playerAlias,
                     uuid: fingerprint
                 }).success(function (player) {
-                    test.equals(player.name, playerAlias);
-                    test.equals(player.uuid, fingerprint);
                     callback();
                 }).failure(function (err) {
                     test.ok(false, err);
@@ -31,21 +29,26 @@ module.exports = {
     testCreate: function (test) {
         sequelize.transaction(function (t) {
             Player.create(
-                { uuid: '1446914010', name: 'Alex' },
+                { uuid: 1446914010, alias: 'Alex' },
                 { transaction: t }
             ).success(function () {
+
                     Player.all({ transaction: t })
                         .success(function (players) {
                             test.equals(players.length, 2);
+                            test.equals(players[1].alias, 'Alex');
+                            test.equals(players[1].uuid, 1446914010);
                             test.done();
                         }).failure(function (err) {
                             test.ok(false, err);
                             test.done();
                         });
+
                 }).failure(function (err) {
                     test.ok(false, err);
                     test.done();
                 })
+
         });
     }
 };
