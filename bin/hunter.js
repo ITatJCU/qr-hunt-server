e9a17480797d5452e5a8716a31a564aaa203319d
Alex <Alex@SKYCATCHER.(none)>
2014-08-13 16:40:40 +1000

    Restify Init Logic

--- bin/hunter.js
+++ bin/hunter.js
@@ -1,1 +1,47 @@
+var FS = require('fs');
+var config = require('./config');
+var server = require('./config/restify');
+require('./config/loggers')(server);
+var models = require('./src/dao/Models');
 
+var LogEventDispatcher = require('./src/utilities/LogEventDispatcher');
+
+function initialiseDatabase() {
+    LogEventDispatcher.log('Initialising database...')
+
+    models.sequelize
+        .sync({ force: true })
+        .complete(function (err) {
+            if (!!err) {
+                LogEventDispatcher.log('\tError initialising database:');
+                LogEventDispatcher.log(err);
+            } else {
+                initialiseServer();
+            }
+        });
+}
+
+function initialiseServer() {
+    LogEventDispatcher.log('Initialising API Server...');
+    server.listen(config.application.port, function (err) {
+        if (!!err) {
+            LogEventDispatcher.log('\tError initialising server:');
+            LogEventDispatcher.log(err);
+        } else {
+            LogEventDispatcher.log('\t' + server.name + ' listening at ' + server.url);
+            initialiseRoutes();
+        }
+    });
+}
+
+function initialiseRoutes() {
+    LogEventDispatcher.log('Loading routes...');
+    var routes = __dirname + '/src/routes';
+
+    FS.readdirSync(routes).forEach(function (file) {
+        LogEventDispatcher.log('\t' + file);
+        require(routes + '/' + file)(server);
+    });
+}
+
+initialiseDatabase();
