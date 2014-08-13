d80a88a333c03a86bd8c07ec72af235230af7cd4
Alex <Alex@SKYCATCHER.(none)>
2014-08-13 16:30:25 +1000

    StrategyLogger Init

---
+++ test/utilities/test-strategy-logger.js
@@ -0,0 +1,10 @@
+var Recorder = require('../../lib/utilities/recorder');
+var Logger = require('../../lib/utilities/strategy-logger');
+var LoggerStrategy = require('../../lib/utilities/logger-strategy');
+var recorder = new Recorder();
+
+exports['testConstructor'] = function (test) {
+    var l = new Logger();
+    test.ok(LoggerStrategy.prototype.isPrototypeOf(l.strategy()));
+    test.done();
+};
